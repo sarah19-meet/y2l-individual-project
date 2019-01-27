69a78c5ea44f7bf85f67a8180cc62c2be5d3d0d3
aboud20-meet <aboud20@meet.mit.edu>
2019-01-27 16:26:19 +0200

    usdhi

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World test!'
+def home():
+    return render_template('homepage.html')
 
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
+    
 if __name__ == '__main__':
     app.run(debug=True)
 
+
+ 
--- database.py
+++ database.py
@@ -10,3 +10,45 @@ session = DBSession()
 
 def function(parameter):
     pass
+
+
+#####################################################3
+
+
+
+# Database related imports
+# Make sure to import your tables!
+from model import Base, Donate
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import literal
+
+
+# You can change the name of your database, just change project.db to whatever you want (make sure to include .db at the end!)
+# Make sure you have the same name for the database in the app.py file!
+engine = create_engine('sqlite:///project.db', connect_args={'check_same_thread': False})
+Base.metadata.create_all(engine)
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+# Your database functions are located under here (querying, adding items, etc.)
+
+# Example of adding a student:
+def add_donate(name,story,email, needer_type, needs, phone_num, address, link, pic):
+    print("Added a donate!")
+    don = Donate(name=name,story=story, email=email, needer_type=needer_type, needs=needs,phone_num=phone_num,address=address,link=link, pic=pic)
+    session.add(don)
+    session.commit()
+
+def get_all_donates():
+    donates = session.query(Donate).all()
+    return donates
+
+def get_all_donates_by_type(type):
+	return session.query(Donate).filter(Donate.needs.contains(type)).all()
+def search_donate(search):
+	results = session.query(Donate).filter(Donate.name.contains(search)).all()
+	return results
+
+gilad=add_donate("gilad" , "i want to buy ice cream" , "g@gmail.com" , "person", "money", 000, "ha","rf", "ddfsdf")
